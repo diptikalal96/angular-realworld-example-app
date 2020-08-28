852d70fe11070391a83bf40f70f1675df7dba6f4
diptikalal96 <54762665+diptikalal96@users.noreply.github.com>
2020-08-28 17:45:42 +0530

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -30,7 +30,7 @@ module.exports = function(config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless'],
+    browsers: ['--headless', '--disable-gpu', '--no-sandbox', '--disable-extensions', '--disable-dev-shm-usage'],
     singleRun: false
   });
 };
